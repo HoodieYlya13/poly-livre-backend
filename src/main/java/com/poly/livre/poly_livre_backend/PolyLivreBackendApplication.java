6b82716711c580b0e1bcffbc8dc38a3a5c7e1513
Ylya <ylyamartchenko@gmail.com>
2025-11-27 15:33:51 +0100

    Initial commit

---
+++ src/main/java/com/poly/livre/poly_livre_backend/PolyLivreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.poly.livre.poly_livre_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyLivreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyLivreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poly/livre/poly_livre_backend/PolyLivreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.livre.poly_livre_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyLivreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
